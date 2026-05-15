a960b889f47ffced030ab9cc9d1ef601f3ba1c97
Brad <44570552+bizojs@users.noreply.github.com>
2026-05-15 14:49:50 +0100

    feat: openGraph tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { page } from "$app/state";
 	import "./layout.css"
     import "@fontsource-variable/ibm-plex-sans/wght.css"
     import { ModeWatcher } from "mode-watcher"
@@ -7,6 +8,18 @@
 </script>
 
 <ModeWatcher />
+
+<svelte:head>
+    <meta name="description" content="Fluxer Testers bug report data visualized into graphs/charts" />
+    <meta property="og:title" content="Fluxer Canary" />
+    <meta property="og:description" content="Fluxer Testers bug report data visualized into graphs/charts" />
+    <meta property="og:image:type" content="image/jpeg">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta property="og:image:width" content="860">
+    <meta property="og:image:height" content="388">
+    <meta property="og:image" content={page.url.origin + "/demo.png"} />
+    <meta property="og:url" content={page.url.href}>
+</svelte:head>
 
 <div class="w-dvw min-h-screen overflow-x-hidden">
     {@render children()}
